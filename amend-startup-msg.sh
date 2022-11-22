5b0188d63b38ed987e1302437bf7046281c7972b
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-11-22 16:29:09 +0100

    Reduce examples to only device virtual (#6)

---
+++ amend-startup-msg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script is to replace the Startup Message from default config files to
+# ease testing.
+
+path=$1
+
+find $path -type f -name 'configuration.toml' | xargs \
+    sed --in-place --regexp-extended \
+    's/StartupMsg.*/StartupMsg="CONFIG BY EXAMPLE PROVIDER"/'
--- examples/app-service-configurable/res/README.md
+++
@@ -1,2 +0,0 @@
-# App Service Configurable Config
-This directory contains the example configuration files that are provided to a consumer `edgex-app-service-configurable` snap.
